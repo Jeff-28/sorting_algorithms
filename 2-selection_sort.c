68be769d2450cf05e97bbb84a056721dc6934a7a
Jeff Martinez <2408@holbertonschool.com>
2021-01-29 21:12:57 +0000

    Adding function that sorts array of ints using Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ *					order using the Selection sort algorithm
+ * @array: the array to be sorted
+ * @size: the size of the array
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	int i, j, min, len, swap;
+
+	for (len = 0; array[len] != '\0'; len++)
+		;
+	for (i = 0; i < len; i++)
+	{
+		min = i;
+		for (j = i + 1; j < len; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			swap = array[min];
+			array[min] = array[i];
+			array[i] = swap;
+			print_array(array, size);
+		}
+	}
+}
