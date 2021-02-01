f6e890a9400ddb6ac330f9551aa52299933b4531
angel19951 <2384@holbertonschool.com>
2021-02-01 14:57:43 +0000

    Bubble sort algorithm | Sorting algorithms & Big O | C

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order using Bubble
+ * Sort Algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ *
+ * Return: no return for this void function
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned int temp, count, finder;
+	if (size > 2)
+	{
+		if (*array != '\0' && size > 2)
+		{
+			for (count = 0; count < (size - 1); count++)
+			{
+				for (finder = 0; finder < (size - 1); finder++)
+				{
+					if (array[finder] > array[finder + 1])
+					{
+						temp = array[finder];
+						array[finder] = array[finder + 1];
+						array[finder + 1] = temp;
+						print_array(array, size);
+					}
+				}
+			}
+		}
+	}
+}
