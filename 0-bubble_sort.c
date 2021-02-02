f4bf0453edce8d05157d5a816374f29fe2d82c98
angel19951 <2384@holbertonschool.com>
2021-02-02 17:32:08 +0000

    Fixing code for checks

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-#include "sort.h"
+=#include "sort.h"
 
 /**
  * bubble_sort - sorts an array of integers in ascending order using Bubble
@@ -15,19 +15,16 @@ void bubble_sort(int *array, size_t size)
 	if (size <= 2)
 		return;
 
-	if (*array != '\0')
+	for (count = 0; count < (size - 1); count++)
 	{
-		for (count = 0; count < (size - 1); count++)
+		for (finder = 0; finder < (size - 1); finder++)
 		{
-			for (finder = 0; finder < (size - 1); finder++)
+			if (array[finder] > array[finder + 1])
 			{
-				if (array[finder] > array[finder + 1])
-				{
-					temp = array[finder];
-					array[finder] = array[finder + 1];
-					array[finder + 1] = temp;
-					print_array(array, size);
-				}
+				temp = array[finder];
+				array[finder] = array[finder + 1];
+				array[finder + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
