c6591dc5806ba6270e662dc40e358ffa62995389
Jeff Martinez <2408@holbertonschool.com>
2021-02-03 21:23:09 +0000

    Simplified the code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,22 +1,22 @@
 #include "sort.h"
 
-void my_swap(int *arr, size_t a, size_t b);
-
 /**
  * selection_sort - sorts an array of integers in ascending
  *					order using the Selection sort algorithm
  * @array: the array to be sorted
  * @size: the size of the array
+ *
  * Return: nothing
  */
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
+	int temp;
 
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
 		for (j = i + 1; j < size; j++)
@@ -26,22 +26,10 @@ void selection_sort(int *array, size_t size)
 		}
 		if (min != i)
 		{
-			my_swap(array, min, i);
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
 			print_array(array, size);
 		}
 	}
-}
-/**
- * my_swap - swaps two values
- * @arr: array of integers
- * @a: index of value to swap
- * @b: index of value to swap
- * Return: nothing
- */
-void my_swap(int *arr, size_t a, size_t b)
-{
-	int temp = arr[a];
-
-	arr[a] = arr[b];
-	arr[b] = temp;
 }
